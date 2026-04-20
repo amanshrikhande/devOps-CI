29ebad51092826c9d011d2e61980b121cb285529
Aman Shrikhande <amanshrikhande7909@gmail.com>
2026-04-20 23:46:10 +0530

    feat: added docker-compose for multi-container setup (app + mongodb)

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 
+// Connect to MongoDB (IMPORTANT: use service name 'mongo')
+mongoose.connect("mongodb://mongo:27017/test")
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log(err));
+
 app.get("/", (req, res) => {
-    res.send("CI/CD Working!");
+    res.send("App + MongoDB running!");
 });
 
 app.listen(3000, () => console.log("Server running"));
